9f23fe28fc6a2bbfb227bdfc51586540b69ec5b8
Jonatan Frederiksen <118982079+JonatanFrederiksenStandoutmedia@users.noreply.github.com>
2024-02-23 00:07:09 +0100

    new frontpage

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,17 @@ import Navmenu from './components/Navmenu.vue';
 </script>
 <style>
 .gradient {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  background: linear-gradient(45deg, #264653, #2a9d8f, #e9c46a, #f4a261, #e76f51);
-  background-size: 600% 100%;
-  animation: gradient 18s linear infinite;
-  animation-direction: alternate;
+    background: linear-gradient(45deg, #264653, #2a9d8f, #e9c46a, #f4a261, #e76f51);
+    background-size: 600% 100%;
+    animation: gradient 18s linear infinite;
+    animation-direction: alternate;
+    width: 100%;
+    max-width: calc(100vw - 6rem);
+    max-height: 600px;
+    border-radius: 3em;
+    margin: 6em  auto 5em;
+    box-shadow: 1px 1px 1px 2px #000;
 }
-
 @keyframes gradient {
   0% {
     background-position: 0%
@@ -22,12 +24,96 @@ import Navmenu from './components/Navmenu.vue';
     background-position: 100%
   }
 }
+
+header {
+  position: fixed;
+  top: 1em;
+  left: 0;
+  right: 0;
+  max-width: min(700px, 100vw - 8rem);
+  margin: 0 auto;
+  z-index: 50;
+}
+
+header>.wrap {
+  max-width: 700px;
+  margin-inline: auto;
+  position: sticky;
+  top: 0;
+  border-radius: 3em;
+  backdrop-filter: blur(2em);
+  background: #0003 !important;
+  color: #fff;
+  padding: .5em .5em .5em 2em !important;
+  box-shadow: 0 0 11px -5px #0005;
+  align-items: center;
+}
+
+header .site-title {
+  font-family: "Honk", system-ui;
+  font-size: 2.5em;
+  line-height: 1.25;
+}
+
+header nav ul {
+  display: flex;
+  gap: 1em;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+span.btn a {
+  padding: 1em 1.5em;
+  border-radius: 2em;
+  background: #0003;
+  color: #fff;
+  text-decoration: none;
+  transition: background .3s;
+}
+span.btn a:hover {
+  background-color: #0005;
+}
+
+
+@media screen and (max-width: 479px){
+  header,
+  .gradient{
+    max-width: calc(100vw - 2rem);
+  }
+  header .site-title{
+    font-size: 1.5em;
+  }
+
+  span.btn a{
+    padding: .5em 1em;
+  }
+}
+
 </style>
 <template>
-  <div class="flex bg-slate-200 ">
+  <div class="flex ">
 
     <!-- <Navmenu title="Menu"></Navmenu> -->
 
+    <header>
+      <div class="wrap flex gap-4 justify-between">
+        <a class="site-title" href="/">Jonatan</a>
+
+        <nav>
+          <ul>
+            <!-- <li class=" wp-block-navigation-item wp-block-navigation-link"><a class="wp-block-navigation-item__content"
+                href="#hello"><span class="wp-block-navigation-item__label">Hej</span></a></li>
+            <li class=" wp-block-navigation-item wp-block-navigation-link"><a class="wp-block-navigation-item__content"
+                href="#projects"><span class="wp-block-navigation-item__label">Projekter</span></a></li> -->
+          </ul>
+        </nav>
+
+
+        <span class="btn"><a href="mailto:frederiksen.jonatan@gmail.com?subject=Lad os arbejde sammen!&body=Hej Jonatan! %0D%0A%0D%0AJeg vil høre mere om ...">Kontakt</a></span>
+
+      </div>
+    </header>
 
     <div class="gradient flex items-center justify-center h-screen grow p-4 relative text-white">
       <div class=""></div>
@@ -43,4 +129,5 @@ import Navmenu from './components/Navmenu.vue';
 
       </div>
     </div>
-</div></template>
+  </div>
+</template>
